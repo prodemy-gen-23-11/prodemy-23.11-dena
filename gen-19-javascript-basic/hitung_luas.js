62e9343f819c41c89c62a73b33908ff4ffa495c0
dena-adryan <48337990+dena-adryan@users.noreply.github.com>
2024-03-06 12:13:55 +0700

    "menambahkan folder tugas javascript basic"

---
+++ gen-19-javascript-basic/hitung_luas.js
@@ -0,0 +1,20 @@
+function luasPersegi(sisi){
+    return sisi*sisi;
+}
+
+function luasSegitiga(alas,tinggi){
+    return alas*tinggi/2;
+}
+
+function luasPersegiPanjang(panjang,lebar){
+    return panjang*lebar;
+}
+
+function luasLingkaran(radius){
+    return 22/7*radius*radius;
+}
+
+console.log(luasPersegi(7));
+console.log(luasSegitiga(4,7));
+console.log(luasPersegiPanjang(4,7));
+console.log(luasLingkaran(7));
